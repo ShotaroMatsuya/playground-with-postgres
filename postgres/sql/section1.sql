bff1a9f48834601a42570aa15c6ec6e4e0d4263c
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2021-11-23 17:26:03 +0900

    first commit

---
+++ postgres/sql/section1.sql
@@ -0,0 +1,29 @@
+DROP DATABASE section1;
+CREATE DATABASE section1;
+USE section1;
+CREATE TABLE cities(
+    name VARCHAR(50),
+    country VARCHAR(50),
+    population INTEGER,
+    area INTEGER
+);
+INSERT INTO cities (name, country, population, area)
+VALUES('Tokyo', 'Japan', '38505000', 8223);
+INSERT INTO cities(name, country, population, area)
+VALUES('Delhi', 'India', 28125000, 2240),
+    ('Shanghai', 'China', 22125000, 4015),
+    ('Sao Paulo', 'Brazil', 20935000, 3043);
+SELECT area,
+    name,
+    population
+FROM cities;
+SELECT name,
+    population / area AS population_density
+FROM cities;
+-- String operator & Function
+SELECT name || ', ' || country AS location
+FROM cities;
+SELECT CONCAT(name, ', ', countrt) AS location
+FROM cities;
+SELECT CONCAT(UPPER(name), ', ', UPPER(countrt)) AS location
+FROM cities;
